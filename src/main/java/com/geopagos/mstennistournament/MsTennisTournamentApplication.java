67f1534896c8ffdeb21b1a45bf55ea20ea723321
Gastón Bonanno <gbonanno-dev@gmail.com>
2024-02-09 18:24:19 -0300

    Init project

---
+++ src/main/java/com/geopagos/mstennistournament/MsTennisTournamentApplication.java
@@ -0,0 +1,13 @@
+package com.geopagos.mstennistournament;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTennisTournamentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTennisTournamentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geopagos/mstennistournament/MsTennisTournamentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geopagos.mstennistournament;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTennisTournamentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
